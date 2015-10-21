45962277ce793fd8601278c1b9e88bd85740c6fb
Stefan <Stefan.Steg@gmail.com>
2015-10-21 09:55:14 +0100

    rm comments

--- basteleiPI/src/basteleiPI.cpp
+++ basteleiPI/src/basteleiPI.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : basteleiPI.cpp
-// Author      : Stefan
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 
