b12cd7d3234bf43d0b538a0aa7a2794a901c2a75
Stefan <Stefan.Steg@gmail.com>
2015-10-22 16:05:17 +0100

    add adafruit_pyhton_dht library

--- basteleiPI/Debug/src/subdir.mk
+++ basteleiPI/Debug/src/subdir.mk
@@ -7,9 +7,31 @@ CPP_SRCS += \
 ../src/GpioHelper.cpp \
 ../src/basteleiPI.cpp 
 
+C_SRCS += \
+../src/_Raspberry_Pi_2_Driver.c \
+../src/_Test_Driver.c \
+../src/common_dht_read.c \
+../src/pi_2_dht_read.c \
+../src/pi_2_mmio.c \
+../src/test_dht_read.c 
+
 OBJS += \
 ./src/GpioHelper.o \
-./src/basteleiPI.o 
+./src/_Raspberry_Pi_2_Driver.o \
+./src/_Test_Driver.o \
+./src/basteleiPI.o \
+./src/common_dht_read.o \
+./src/pi_2_dht_read.o \
+./src/pi_2_mmio.o \
+./src/test_dht_read.o 
+
+C_DEPS += \
+./src/_Raspberry_Pi_2_Driver.d \
+./src/_Test_Driver.d \
+./src/common_dht_read.d \
+./src/pi_2_dht_read.d \
+./src/pi_2_mmio.d \
+./src/test_dht_read.d 
 
 CPP_DEPS += \
 ./src/GpioHelper.d \
@@ -20,7 +42,14 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I/usr/include/python2.7 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
