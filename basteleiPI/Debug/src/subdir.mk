11cb585304e1ff632bc41869d3a17e265995b41b
Stefan <Stefan.Steg@gmail.com>
2015-11-03 12:56:44 +0000

    enh README

--- basteleiPI/Debug/src/subdir.mk
+++ basteleiPI/Debug/src/subdir.mk
@@ -39,7 +39,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -include/home/GordonShumway/git/basteleiPI/basteleiPI/src/test_dht_read.h -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
